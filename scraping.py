251f1de96c003da9dce02df4c85a870e3f0c9a5f
tomaszbilka <bila4@op.pl>
2024-11-12 11:43:23 +0100

    add scraping on home page

---
+++ scraping.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+all_quotes = []
+
+res = requests.get("https://quotes.toscrape.com/")
+
+soup = BeautifulSoup(res.text)
+quotes = soup.find_all(class_ = "quote")
+
+for quote in quotes:
+    all_quotes.append({
+        "text": quote.find(class_ = "text").get_text(),
+        "author": quote.find(class_ = "author").get_text(),
+        "bio-link": quote.find("a")["href"]
+    })
+    
+print(all_quotes)
