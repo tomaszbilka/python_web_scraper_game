8392edcf5b065f3b59b58ee13d7efecc31968aad
tomaszbilka <bila4@op.pl>
2024-11-13 09:59:49 +0100

    add loop

--- scraping.py
+++ scraping.py
@@ -2,17 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 
 all_quotes = []
+base_url = "https://quotes.toscrape.com/"
+url = "/page/1"
 
-res = requests.get("https://quotes.toscrape.com/")
+while url:
+    res = requests.get(f"{base_url}{url}")
+    print(f"SCRAPING: ---- >>> {base_url}{url}")
+    soup = BeautifulSoup(res.text, "html.parser")
+    quotes = soup.find_all(class_ = "quote")
 
-soup = BeautifulSoup(res.text)
-quotes = soup.find_all(class_ = "quote")
+    for quote in quotes:
+        all_quotes.append({
+            "text": quote.find(class_ = "text").get_text(),
+            "author": quote.find(class_ = "author").get_text(),
+            "bio-link": quote.find("a")["href"]
+        })
 
-for quote in quotes:
-    all_quotes.append({
-        "text": quote.find(class_ = "text").get_text(),
-        "author": quote.find(class_ = "author").get_text(),
-        "bio-link": quote.find("a")["href"]
-    })
+    next_btn = soup.find(class_="next")     
+    url = next_btn.find("a")["href"] if next_btn else None
     
-print(all_quotes)
