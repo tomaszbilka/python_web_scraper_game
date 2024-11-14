6b9a6200c916dbb66736c00578adff57ed1a2572
tomaszbilka <bila4@op.pl>
2024-11-14 08:14:30 +0100

    self refactor

--- scraping.py
+++ scraping.py
@@ -3,29 +3,31 @@ from bs4 import BeautifulSoup
 from time import sleep
 from random import choice
 
-all_quotes = []
-base_url = "https://quotes.toscrape.com/"
-url = "/page/1"
+BASE_URL = "https://quotes.toscrape.com"
 
-while url:
-    res = requests.get(f"{base_url}{url}")
-    print(f"SCRAPING: ---- >>> {base_url}{url} ...")
-    soup = BeautifulSoup(res.text, "html.parser")
-    quotes = soup.find_all(class_ = "quote")
+def scrafe_quotes():
+    all_quotes = []
+    url = "/page/1"
+    while url:
+        res = requests.get(f"{BASE_URL}{url}")
+        print(f"SCRAPING: ---- >>> {BASE_URL}{url} ...")
+        soup = BeautifulSoup(res.text, "html.parser")
+        quotes = soup.find_all(class_ = "quote")
 
-    for quote in quotes:
-        all_quotes.append({
-            "text": quote.find(class_ = "text").get_text(),
-            "author": quote.find(class_ = "author").get_text(),
-            "bio-link": quote.find("a")["href"]
-        })
+        for quote in quotes:
+            all_quotes.append({
+                "text": quote.find(class_ = "text").get_text(),
+                "author": quote.find(class_ = "author").get_text(),
+                "bio-link": quote.find("a")["href"]
+            })
 
-    next_btn = soup.find(class_="next")     
-    url = next_btn.find("a")["href"] if next_btn else None
-    # sleep(2) # prevent from overloading the server
+        next_btn = soup.find(class_="next")     
+        url = next_btn.find("a")["href"] if next_btn else None
+        # sleep(2) # prevent from overloading the server
+    return all_quotes
 
-def start_game():
-    quote = choice(all_quotes)
+def start_game(quotes):
+    quote = choice(quotes)
     remaining_quesses = 4
     guess = ''
 
@@ -39,7 +41,7 @@ def start_game():
             break
         remaining_quesses -= 1
         if(remaining_quesses == 3):
-            res = requests.get(f"{base_url}{quote['bio-link']}")
+            res = requests.get(f"{BASE_URL}{quote['bio-link']}")
             soup = BeautifulSoup(res.text, "html.parser")
             birth_date = soup.find(class_="author-born-date").get_text()
             birth_place = soup.find(class_="author-born-location").get_text()
@@ -58,8 +60,9 @@ def start_game():
     while again not in ('y', 'yes', 'n', 'no'):
         again = input("Do you want to play again? [y/n] \n")
     if again.lower() in ('y','yes'):
-        return start_game()
+        return start_game(quotes)
     else:
         print("Thanks for playing!")
 
-start_game()
+quotes = scrafe_quotes()
+start_game(quotes)
