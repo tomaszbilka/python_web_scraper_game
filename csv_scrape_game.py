1ed119a3323c851084542cc2b45ea6b30756984d
tomaszbilka <bila4@op.pl>
2024-11-15 14:19:03 +0100

    add csv game option

--- csv_scrape_game.py
+++ csv_scrape_game.py
@@ -1,30 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
+from csv import DictReader
 from random import choice
 
 BASE_URL = "https://quotes.toscrape.com"
 
-def scrape_quotes():
-    all_quotes = []
-    url = "/page/1"
-    while url:
-        res = requests.get(f"{BASE_URL}{url}")
-        print(f"SCRAPING: ---- >>> {BASE_URL}{url} ...")
-        soup = BeautifulSoup(res.text, "html.parser")
-        quotes = soup.find_all(class_ = "quote")
+def read_quotes(filename):
+    with open(filename, "r") as file:
+        csv_reader = DictReader(file)
+        return list(csv_reader)
 
-        for quote in quotes:
-            all_quotes.append({
-                "text": quote.find(class_ = "text").get_text(),
-                "author": quote.find(class_ = "author").get_text(),
-                "bio-link": quote.find("a")["href"]
-            })
-
-        next_btn = soup.find(class_="next")     
-        url = next_btn.find("a")["href"] if next_btn else None
-        # sleep(2) # prevent from overloading the server
-    return all_quotes
 
 def start_game(quotes):
     quote = choice(quotes)
@@ -64,5 +49,5 @@ def start_game(quotes):
     else:
         print("Thanks for playing!")
 
-quotes = scrape_quotes()
+quotes = read_quotes("quotes.csv")
 start_game(quotes)
